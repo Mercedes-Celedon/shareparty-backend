6234e1008dfd071e42631c9ec4c73224b225ac75
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-09-03 04:45:09 -0600

    adding a spring jpa proyect

---
+++ src/main/java/com/shareparty/shareparty_backend/SharepartyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shareparty.shareparty_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharepartyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharepartyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shareparty/shareparty_backend/SharepartyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareparty.shareparty_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharepartyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
